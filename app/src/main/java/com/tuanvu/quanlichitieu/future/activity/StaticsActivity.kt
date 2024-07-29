5c53a07e2cee709101de73d515549f33a74287d7
Lê Tuấn Vũ <vult@its-global.vn>
2024-07-29 23:25:52 +0700

    thống kê

---
+++ app/src/main/java/com/tuanvu/quanlichitieu/future/activity/StaticsActivity.kt
@@ -0,0 +1,14 @@
+package com.tuanvu.quanlichitieu.future.activity
+
+import com.tuanvu.quanlichitieu.base.BaseActivity
+import com.tuanvu.quanlichitieu.databinding.ActivityStatiticsBinding
+
+class StaticsActivity :BaseActivity<ActivityStatiticsBinding>()  {
+    override fun getViewBinding(): ActivityStatiticsBinding {
+        return ActivityStatiticsBinding.inflate(layoutInflater)
+    }
+
+    override fun createView() {
+
+    }
+}
